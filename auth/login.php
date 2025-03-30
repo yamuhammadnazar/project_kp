0ae88f12cc9f2c9e5402f610432d1233c64509e9
yamuhammadnazar <yamuhammadnazar@gmail.com>
2025-03-30 15:15:02 +0700

    memperbaharui script file login.js

--- auth/login.php
+++ auth/login.php
@@ -96,6 +96,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <!-- Bootstrap 5.3.2 JS Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- Custom JS -->
-    <script src="../assets/js/login.js"></script>
+    <script src="../assets/js/auth/login.js"></script>
 </body>
 </html>
